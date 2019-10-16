1448ded996d74935090a7ff6cfc7990d26b6b075
Umesh Chandra <UmeshC@hcl.com>
2019-10-16 18:27:52 +0530

    component modified

--- react-movie-app/src/components/Home.js
+++ react-movie-app/src/components/Home.js
@@ -7,16 +7,30 @@ import Grid from "./elements/Grid";
 import MovieThum from "./elements/MovieThum";
 import LoadMoreBtn from "./elements/LoadMoreBtn";
 import Spinner from "./elements/Spinner";
+import {
+  API_URL,
+  API_KEY,
+  API_BASE_URL,
+  POSTER_SIZE,
+  BACKDROP_SIZE
+} from "../config";
 
-const Home = () => (
-  <>
-    <HeroImage />
-    <SearchBar />
-    <Grid />
-    <MovieThum />
-    <Spinner />
-    <LoadMoreBtn />
-  </>
-);
+import { useState, useEffect } from "react";
+
+const Home = () => {
+  const [state, setState] = useState({ movies: [] });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  return (
+    <>
+      <HeroImage />
+      <SearchBar />
+      <Grid />
+      <MovieThum />
+      <Spinner />
+      <LoadMoreBtn />
+    </>
+  );
+};
 
 export default Home;
---
+++ react-movie-app/src/config.js
@@ -0,0 +1,6 @@
+const API_URL = "https://api.themoviedb.org/3/";
+const API_KEY = "2cc3426f7f061fcf4735b8884e81c54f";
+const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
+const BACKDROP_SIZE = "w1280";
+const POSTER_SIZE = "w500";
+export { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE };
